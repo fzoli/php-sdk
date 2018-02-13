a3f49aa5753bb9eed3551de1ea6407599b81accc
Zoltan Farkas <f.zoli@mailbox.hu>
2018-02-13 10:06:55 +0100

    two-level serialization test

--- src/test_serializer.php
+++ src/test_serializer.php
@@ -11,6 +11,7 @@ use Symfony\Component\Serializer\Serializer;
 class User {
 
     private $name;
+    private $personName;
     private $createDate;
 
     public static function builder(): User {
@@ -29,12 +30,53 @@ class User {
         return $this;
     }
 
+    public function getPersonName(): PersonName {
+        return $this->personName;
+    }
+
+    public function setPersonName(PersonName $name): User {
+        $this->personName = $name;
+        return $this;
+    }
+
     public function getCreateDate(): DateTime {
         return $this->createDate;
     }
 
     public function setCreateDate(DateTime $createDate): User {
         $this->createDate = $createDate;
+        return $this;
+    }
+
+}
+
+class PersonName {
+
+    private $firstName;
+    private $lastName;
+
+    public static function builder(): PersonName {
+        return new PersonName();
+    }
+
+    private function __construct() {
+    }
+
+    public function getFirstName(): string {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $name): PersonName {
+        $this->firstName = $name;
+        return $this;
+    }
+
+    public function getLastName(): string {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $name): PersonName {
+        $this->lastName = $name;
         return $this;
     }
 
@@ -53,13 +95,12 @@ $s = new Serializer(
 
 $user1 = User::builder()
     ->setName("Zoli")
+    ->setPersonName(PersonName::builder()
+        ->setFirstName("A")
+        ->setLastName("B"))
     ->setCreateDate(new DateTime());
 
-$user2 = User::builder()
-    ->setName("Zoli")
-    ->setCreateDate(new DateTime());
-
-$jsonContent = $s->serialize(array($user1, $user2), 'json');
+$jsonContent = $s->serialize(array($user1), 'json');
 echo $jsonContent;
 
 ?>
