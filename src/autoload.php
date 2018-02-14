0e0a4fc670ab29d0f672eff206165660a134c2c3
Zoltan Farkas <f.zoli@mailbox.hu>
2018-02-14 12:13:45 +0100

    use composer autoload

--- src/autoload.php
+++ src/autoload.php
@@ -1,10 +1,2 @@
 <?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-spl_autoload_register(function ($className) {
-    $file = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-    if (is_file($file)) {
-        include_once $file;
-    }
-});
+require __DIR__ . '/../vendor/autoload.php';
